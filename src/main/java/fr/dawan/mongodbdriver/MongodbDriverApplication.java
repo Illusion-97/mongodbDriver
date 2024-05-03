66ed3c8f0d2fe37c73b3dca42ae94c2ca85b7a67
Yanis ADEKALOM <yadekalom@gmail.com>
2024-05-03 14:06:19 +0200

    Initial commit

---
+++ src/main/java/fr/dawan/mongodbdriver/MongodbDriverApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.mongodbdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbDriverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dawan/mongodbdriver/MongodbDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.mongodbdriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
