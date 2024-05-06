d763a2b55d25ef7585b90966993c9948fab1393b
Yanis ADEKALOM <yadekalom@gmail.com>
2024-05-06 11:37:23 +0200

    Feat : InventoryController (Filters & Updates)

--- src/main/java/fr/dawan/mongodbdriver/typed/InventoryController.java
+++ src/main/java/fr/dawan/mongodbdriver/typed/InventoryController.java
@@ -2,14 +2,19 @@ package fr.dawan.mongodbdriver.typed;
 
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.InsertManyResult;
 import com.mongodb.client.result.InsertOneResult;
+import com.mongodb.client.result.UpdateResult;
 import fr.dawan.mongodbdriver.conf.ConfigurationManager;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.pojo.PojoCodecProvider;
+import org.bson.conversions.Bson;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,9 +60,21 @@ public class InventoryController {
         return collection.find(new Document("item", item)).into(new ArrayList<>());
     }
 
-    /*@GetMapping("findByStatusAndQtyLt/{status}/{qty}")
-    public List<Inventory> findByStatusAndQtyLessThan() {
+    @GetMapping("findByStatusAndQtyLt/{status}/{qty}")
+    public List<Inventory> findByStatusAndQtyLessThan(@PathVariable String qty, @PathVariable String status) {
 
-    }*/
+        return collection.find(Filters.and(
+                Filters.eq("status",status),
+                Filters.lt("qty", qty)
+        )).into(new ArrayList<>());
+    }
 
+
+    @PutMapping("increaseQty/{increase}")
+    public UpdateResult increaseQty(@PathVariable int increase) {
+        // Pensez à verifier si une classe utilitaire n'existe pas déjà pour ce que vous souhaitez faire.
+        return collection.updateMany(
+                new Document(), // Quand une mise à jour, le filtre ne doit jamais être null, si vous souhaitez tout mettre à jour, il faut un document vide
+                Updates.inc("qty", increase));
+    }
 }
